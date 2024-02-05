7e3c15a925d5ff89660007b34321b393184ea519
Francois <francoisvans@gmail.com>
2024-02-05 20:26:56 +0100

    stuling

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -1,5 +1,5 @@
 import UIComponent from "sap/ui/core/UIComponent";
-import { support } from "sap/ui/Device";
+import Device from "sap/ui/Device";
 import models from "./model/models";
 
 /**
@@ -30,18 +30,13 @@ export default class Component extends UIComponent {
 	 * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
 	 */
 	public getContentDensityClass(): string {
-		if (this.contentDensityClass === undefined) {
-			// check whether FLP has already set the content density class; do nothing in this case
-			if (document.body.classList.contains("sapUiSizeCozy") || document.body.classList.contains("sapUiSizeCompact")) {
-				this.contentDensityClass = "";
-			} else if (!support.touch) {
-				// apply "compact" mode if touch is not supported
+		if (!this.contentDensityClass) {
+			if (!Device.support.touch){
 				this.contentDensityClass = "sapUiSizeCompact";
 			} else {
-				// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
 				this.contentDensityClass = "sapUiSizeCozy";
 			}
-		}
+		} 
 		return this.contentDensityClass;
 	}
 }
